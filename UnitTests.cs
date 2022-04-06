6ecb8ab9dedfe83e2d1f9723577af071b0084a91
Flavia <44437173+msflaav@users.noreply.github.com>
2022-04-06 18:26:00 +0100

    FB- add unit test for calculateDimensions for parcels -failing

--- UnitTests.cs
+++ UnitTests.cs
@@ -1,10 +1,44 @@
 using Xunit;
 
-public class testclass 
+public class testclass
 {
     [Fact]
-    public void SampleTest()
-    {
 
+    public void calculateDimensionTestSmallParcel()
+    {
+        double length = 2;
+        double height = 1;
+        double width = 3;
+        double weight = 1;
+        Assert.Equal("Small Parcel: $3.Total Cost: $3", Program.calculateDimension(length, height, width, weight));
     }
+    [Fact]
+
+    public void calculateDimensionTestMediumParcel()
+    {
+        double length = 4;
+        double height = 4;
+        double width = 3;
+        double weight = 2;
+        Assert.Equal("Medium Parcel: $8.Total Cost: $8", Program.calculateDimension(length, height, width, weight));
+    }
+    [Fact]
+    public void calculateDimensionTestLargeParcel()
+    {
+        double length = 5;
+        double height = 5;
+        double width = 3;
+        double weight = 2;
+        Assert.Equal("Large Parcel: $15.Total Cost: $15", Program.calculateDimension(length, height, width, weight));
+    }
+    [Fact]
+    public void calculateDimensionTestXLargeParcel()
+    {
+        double length = 19;
+        double height = 12;
+        double width = 10;
+        double weight = 2;
+        Assert.Equal("Extra Large Parcel: $25.Total Cost: $25", Program.calculateDimension(length, height, width, weight));
+    }
+
 }
